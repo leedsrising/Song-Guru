e0d6320106093f2f7ad12e64e09f771977bfb937
Leeds Rising <lar294@cornell.edu>
2018-07-11 20:50:02 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+##Twitter keys
+
+# Consume:
+TWITTER_CONSUMER_KEY    = ""
+TWITTER_CONSUMER_SECRET = ""
+
+# Access:
+TWITTER_ACCESS_TOKEN  = ""
+TWITTER_ACCESS_SECRET = ""
+
+##Spotify keys
+
+# Client:
+SPOTIFY_CLIENT_ID = ""
+SPOTIFY_CLIENT_SECRET = ""
+
+# Redirect URI - to be determined from spotify API usage:
+SPOTIFY_REDIRECT_URI = ""
+
+# Personal spotify account username:
+SPOTIFY_USERNAME = ""
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
+
 import tweepy
 import pandas as pd
 import numpy as np
 from IPython.display import display
-import matplotlib.pyplot as plt
-import seaborn as sns
 import re
 import copy
 import operator
@@ -13,6 +13,7 @@ import spotipy.util as util
 import datetime
 import json
 import config
+import spotify
 
 def twitter_setup():
     """
@@ -31,7 +32,12 @@ def twitter_setup():
 extractor = twitter_setup()
 
 # Creating hillydilly tweet list
-hillydilly_tweets = extractor.user_timeline(screen_name="hillydilly", count=200, include_rts = False, tweet_mode = "extended")
+hillydilly_tweets = extractor.user_timeline(
+    screen_name="hillydilly", 
+    count=200, 
+    include_rts = False, 
+    tweet_mode = "extended"
+    )
 
 #expanding the accepted string length so that tweets will not be truncated
 pd.set_option('max_colwidth',1000)
@@ -58,10 +64,15 @@ def match_quotes_with_dash(text):
         song_start_index = artist_and_song.find("-") + 2
         return (artist_and_song[song_start_index:])
     raise ValueError("no matches")
+
 #dashes test
 #print(match_quotes_with_dash('asddsa "asdsda - duality" asddas '))
+
 song_dictionary = {}    
     
+# adds the songs from [tweet_dataframe], a pandas dataframe containing song tweets,
+# to the existing [song_dictionary] library by finding songs that are quoted under
+# the format of [quote_type]
 def add_songs_to_dic(song_dictionary, tweet_dataframe, quote_type):
     for i, row in (tweet_dataframe).iterrows():
         tweet = (row["Tweets"])
@@ -73,7 +84,6 @@ def add_songs_to_dic(song_dictionary, tweet_dataframe, quote_type):
                 quoted_words = match_quotes_with_dash(tweet)
             else:
                 raise ValueError("quote_type")
-            print(quoted_words)
             # add the song to the song dictionary
             # if the song is already in the dictionary, give it a +1 occurrence
             if quoted_words in song_dictionary:
@@ -84,3 +94,80 @@ def add_songs_to_dic(song_dictionary, tweet_dataframe, quote_type):
             print(err.args)
 
 add_songs_to_dic(song_dictionary, hillydilly_data, "standard") 
+
+# returns the key of the max value of the dic
+def get_min_dic_key(dic):
+    min = (dic.keys())[0]
+    for key in dic:
+        if dic[key] < dic[min]:
+            min = key
+    return min
+
+# returns dictionary of 10 best song with recalculated values based on number 
+# of likes and retweets
+def get_best_songs(song_dictionary):
+    sd_recalculated = {} 
+    for key in song_dictionary:
+        (sd_recalculated[key]) = (song_dictionary[key])[0] * 7.5 + (song_dictionary[key])[1]
+    # treat "best" as sentiment + recalculated occurrences
+    accum = {}
+    for key in sd_recalculated:
+        if len(accum.keys()) < 10:
+            accum[key] = sd_recalculated[key]
+        else:
+            min = get_min_dic_key(accum)
+            if sd_recalculated[key] > accum[min]:    
+                del accum[min]
+                accum[key] = sd_recalculated[key]
+        return accum
+
+# creating hypemachine tweet list
+hypemachine_tweets = extractor.user_timeline(
+    screen_name="hypem", 
+    count=200, 
+    include_rts = False, 
+    tweet_mode = "extended")
+print("Number of tweets extracted: {}.\n".format(len(hypemachine_tweets)))
+
+# Create a list of hypemachine tweets
+tweetdata = []
+for tweet in hypemachine_tweets:
+    tweetdata.append(tweet.full_text)
+hypemachine_data = pd.DataFrame(tweetdata, columns=['Tweets'])
+hypemachine_data['Likes']  = np.array([tweet.favorite_count for tweet in hypemachine_tweets])
+hypemachine_data['RTs']    = np.array([tweet.retweet_count for tweet in hypemachine_tweets])
+hypemachine_data['General_sentiment'] = hypemachine_data['Likes'] + hypemachine_data['RTs']
+
+add_songs_to_dic(song_dictionary, hypemachine_data, "standard")
+
+# Creating submithub tweet list
+submithub_tweets = extractor.user_timeline(
+    screen_name="submit_hub", 
+    count=1000, 
+    include_rts = False, 
+    tweet_mode = "extended")
+print("Number of tweets extracted from submithub: {}.\n".format(len(submithub_tweets)))
+
+# Create a list of submithub tweets
+tweetdata = []
+for tweet in submithub_tweets:
+    tweetdata.append(tweet.full_text)
+submithub_data = pd.DataFrame(tweetdata, columns=['Tweets'])
+submithub_data['Likes']  = np.array([tweet.favorite_count for tweet in submithub_tweets])
+submithub_data['RTs']    = np.array([tweet.retweet_count for tweet in submithub_tweets])
+submithub_data['General_sentiment'] = submithub_data['Likes'] + submithub_data['RTs']
+
+add_songs_to_dic(song_dictionary, submithub_data, "dashes")
+
+#getting the top 5 best songs from this song dictionary
+best_songs = get_best_songs(song_dictionary)
+print("the best songs: " + str(best_songs))
+# turning the best_songs list of song names into a list of tids of those songs
+tids = spotify.get_tids(best_songs)
+
+# creating a new playlist in the user's spotify library
+spotify.create_playlist()
+
+# add the songs from [tids] to the newly created playlist
+spotify.add_songs_to_playlist(tids)
+
